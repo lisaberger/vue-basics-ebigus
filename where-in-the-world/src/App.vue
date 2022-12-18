e8e4fd069e03bd96c73d22d69e37323628fa8d8d
bergerli <lisa.berger@hs-augsburg.de>
2022-12-18 17:58:36 +0100

    insert header and routerview component into app

--- where-in-the-world/src/App.vue
+++ where-in-the-world/src/App.vue
@@ -1,9 +1,18 @@
-<script>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <RouterView />
+  <the-header />
+  <router-view />
 </template>
+
+<script>
+import { RouterView } from "vue-router";
+import TheHeader from "@/components/TheHeader.vue";
+
+export default {
+  components: {
+    TheHeader,
+    RouterView,
+  },
+};
+</script>
 
 <style scoped></style>
