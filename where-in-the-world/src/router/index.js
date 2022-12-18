25007e8951c325efd2ecb5f86297b885cb0b25dc
bergerli <lisa.berger@hs-augsburg.de>
2022-12-18 17:54:58 +0100

    install axios

--- where-in-the-world/src/router/index.js
+++ where-in-the-world/src/router/index.js
@@ -10,7 +10,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: "/country",
+      path: "/country/:name",
       name: "country",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
