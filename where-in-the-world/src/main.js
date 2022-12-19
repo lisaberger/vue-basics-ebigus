7107d0e1cf76191b595779881667ac3b50dcbcd4
bergerli <lisa.berger@hs-augsburg.de>
2022-12-19 11:32:50 +0100

    install and use font-awesome-icons

--- where-in-the-world/src/main.js
+++ where-in-the-world/src/main.js
@@ -1,11 +1,19 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faMoon,
+  faSearch,
+  faArrowLeft,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-import "./assets/main.css";
+import "./assets/base.css";
+
+library.add(faMoon, faSearch, faArrowLeft);
 
 const app = createApp(App);
-
 app.use(router);
-
 app.mount("#app");
+app.component("FontAwesomeIcon", FontAwesomeIcon);
