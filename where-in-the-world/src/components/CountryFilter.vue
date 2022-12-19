9c2e824e3a6e2e90839893339ef3f9f2f9ac3257
bergerli <lisa.berger@hs-augsburg.de>
2022-12-19 11:37:49 +0100

    rename country filter area to country filter

--- where-in-the-world/src/components/CountryFilter.vue
+++ where-in-the-world/src/components/CountryFilter.vue
@@ -1,13 +1,18 @@
 <template>
   <form class="form">
-    <input
-      id="search"
-      type="search"
-      name="search"
-      placeholder="Search Country"
-    />
+    <div class="search-input">
+      <font-awesome-icon :icon="['fas', 'search']" />
+      <input
+        id="search"
+        type="search"
+        name="search"
+        placeholder="Search for a country..."
+      />
+    </div>
+
     <div class="select">
       <select id="select" name="select">
+        <option>Filter by Region</option>
         <option v-for="region in regions" :key="region.id" :value="region.name">
           {{ region }}
         </option>
@@ -28,14 +33,15 @@ export default {
 </script>
 
 <style scoped>
+@import "../assets/base.css";
 .form {
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
-  padding: 40px 80px;
+  padding: 20px 20px;
 }
-.form input,
+.search-input,
 .form select {
   background-color: var(--color-elements);
   box-shadow: 0 5px 10px hsla(200, 15%, 8%, 0.1);
@@ -43,14 +49,22 @@ export default {
   font-family: inherit;
   border: none;
   border-radius: 5px;
-  color: var(--text-color);
+  color: var(--color-text);
 }
 
 .form input {
-  width: 40rem;
+  min-width: 30vw;
+  border: none;
+  margin-left: 10px;
+}
+.search-input {
+  display: flex;
+  align-items: center;
 }
 
-.form .select select {
-  width: 20rem;
+@media (min-width: 1440px) {
+  .form {
+    padding: 45px 80px;
+  }
 }
 </style>
