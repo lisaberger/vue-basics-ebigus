7d306442d0d5031e3cd38cc158fe7abcccac3d98
bergerli <lisa.berger@hs-augsburg.de>
2022-12-19 11:41:16 +0100

    change header style to be responsive

--- where-in-the-world/src/components/TheHeader.vue
+++ where-in-the-world/src/components/TheHeader.vue
@@ -5,10 +5,10 @@
     </div>
 
     <div>
-      <button className="btn">
-        <i className="fas fa-moon"></i>
-        Dark Mode
-      </button>
+      <div class="btn">
+        <font-awesome-icon :icon="['fas', 'moon']" />
+        <span class="btn__name">Dark Mode</span>
+      </div>
     </div>
   </header>
 </template>
@@ -19,24 +19,46 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+@import "../assets/base.css";
+
+h1 {
+  font-size: 1rem;
+  font-weight: 800;
+}
+
 .header {
-  padding: 10px 80px;
-  min-height: 60px;
+  padding: 20px 20px;
+  min-height: 80px;
   display: flex;
   flex-wrap: wrap;
   align-items: center;
   justify-content: space-between;
-  color: var(--text-color);
+  color: var(--color-text);
   box-shadow: 0 5px 10px hsla(200, 15%, 8%, 0.1);
 }
 
 .btn {
-  background-color: var(--background-color);
+  background-color: var(--color-background);
+  font-weight: 600;
   border: none;
   padding: 5px;
   cursor: pointer;
   text-decoration: none;
   color: var(--color-text);
+}
+
+.btn__name {
+  margin-left: 10px;
+}
+
+@media (min-width: 1440px) {
+  .header {
+    padding: 10px 80px;
+  }
+
+  h1 {
+    font-size: 1.5rem;
+  }
 }
 </style>
